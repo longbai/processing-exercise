bc72b4139585c66f1bff63db7fe3f2c7371b896b
longbai <slongbai@gmail.com>
2014-01-25 16:14:19 +0800

    init

---
+++ src/gswp/E2_2.groovy
@@ -0,0 +1,27 @@
+package gswp
+
+import processing.core.PApplet
+
+/**
+ * Created by bailong on 2014-1-25.
+ */
+class E2_2 extends PApplet{
+    static void main(args){
+        PApplet.main(["E2_2"] as String[])
+    }
+
+    void setup(){
+        size(480, 120)
+        smooth()
+    }
+
+    void draw(){
+        if(mousePressed){
+            fill(0)
+        }else{
+            fill(255)
+        }
+
+        ellipse(mouseX, mouseY, 80,80)
+    }
+}
