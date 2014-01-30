cdf8df98173723db0495ae074bd736a3020db550
longbai <slongbai@gmail.com>
2014-01-30 22:37:12 +0800

    e7 9

--- src/gswp/E7_9.groovy
+++ src/gswp/E7_9.groovy
@@ -10,8 +10,20 @@ class E7_9 extends PApplet {
     void setup() {
         size(480, 120)
         smooth()
+        _x = width/2
+        _y = height/2
+        stroke(color(0, 0, 255))
     }
+    float _x
+    float _y
+    float diameter = 20
+    float speed = 2.5
 
     void draw() {
+        _x += random(-speed, speed)
+        _x = constrain(_x, 0, width)
+        _y += random(-speed, speed)
+        _y = constrain(_y, 0, height)
+        ellipse(_x, _y, diameter, diameter)
     }
 }
