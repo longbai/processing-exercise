1e34692dd4037e18166ccfdc92c10482a239f727
longbai <slongbai@gmail.com>
2014-01-30 22:36:05 +0800

    new exercise

---
+++ src/gswp/E7_9.groovy
@@ -0,0 +1,17 @@
+package gswp
+
+import processing.core.PApplet
+
+class E7_9 extends PApplet {
+    static void main(args) {
+        PApplet.main(['E7_9'] as String[])
+    }
+
+    void setup() {
+        size(480, 120)
+        smooth()
+    }
+
+    void draw() {
+    }
+}
---
+++ src/gswp/Lmu.groovy
@@ -0,0 +1,17 @@
+package gswp
+
+import processing.core.PApplet
+
+class Lmu extends PApplet {
+    static void main(args) {
+        PApplet.main(['Lmu'] as String[])
+    }
+
+    void setup() {
+        size(480, 120)
+        smooth()
+    }
+
+    void draw() {
+    }
+}
